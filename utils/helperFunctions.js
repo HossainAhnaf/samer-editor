d9bf1f05f9a7a6afe67f97212aec864f41eed7c7
HossainAhnaf <itsbuldusmp@gmail.com>
2023-11-23 11:35:19 +0600

    Fixed 1 bug

--- utils/helperFunctions.js
+++ utils/helperFunctions.js
@@ -1,46 +1,45 @@
 // really we need a Built in insertAfter
 export function insertAfter(newNode, child, parent) {
-    if (child.nextElementSibling === null) {
-      parent.appendChild(newNode);
-    } else {
-      parent.insertBefore(newNode, child.nextElementSibling);
-    }
+  if (child.nextElementSibling === null) {
+    parent.appendChild(newNode);
+  } else {
+    parent.insertBefore(newNode, child.nextElementSibling);
   }
-  
-  //helper fn: debounce
-  export function debounce(fn, delay) {
-    let timeout = undefined;
-    return function () {
-      if (timeout) clearTimeout(timeout);
-      timeout = setTimeout(fn, delay);
-    };
-  }
-  
-  // helper function for finding the last element child of any elm
-  export function getDeepestChild(elm){
-    let currentElm = elm;
-    while (
-      currentElm.firstElementChild !== null &&
-      currentElm.firstElementChild.nodeName !== 'BR'
-    ) {
-      currentElm = currentElm.firstElementChild;
-    }
-  
-    return currentElm;
+}
+
+//helper fn: debounce
+export function debounce(fn, delay) {
+  let timeout = undefined;
+  return function () {
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(fn, delay);
   };
-  
-  // helper function for focus the LastTime Focused Node
-  export function focusLastTimeFocusedNode(context) {
-  
-    if (context._focusedOffset > context._focusedTextNode) {
-      context._focusedOffset = context._focusedTextNode.length;
-    }
-    const range =
-      context._selection.rangeCount > 0
-        ? context._selection.getRangeAt(0)
-        : document.createRange();
-    range.setStart(context._focusedTextNode, context._focusedOffset);
-    range.setEnd(context._focusedTextNode, context._focusedOffset);
-    context._selection.removeAllRanges();
-    context._selection.addRange(range);
+}
+
+// helper function for finding the last element child of any elm
+export function getDeepestChild(elm){
+  let currentElm = elm;
+  while (
+    currentElm.firstElementChild !== null &&
+    currentElm.firstElementChild.nodeName !== 'BR'
+  ) {
+    currentElm = currentElm.firstElementChild;
   }
+
+  return currentElm;
+};
+
+// helper function for focus the LastTime Focused Node
+export function focusLastTimeFocusedNode(context) {
+  if (context._focusedOffset > context._focusedTextNode.length) {
+    context._focusedOffset = context._focusedTextNode.length;
+  }
+  const range =
+   (context._selection.rangeCount > 0
+      ? context._selection.getRangeAt(0)
+      : document.createRange());
+  range.setStart(context._focusedTextNode, context._focusedOffset);
+  range.setEnd(context._focusedTextNode, context._focusedOffset);
+  context._selection.removeAllRanges();
+  context._selection.addRange(range);
+}
